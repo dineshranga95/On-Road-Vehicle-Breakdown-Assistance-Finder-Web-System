2b4eac27d56fcaa5930e13f1d18ed8b61783403e
dineshranga95 <dineshranga570@gmail.com>
2020-02-04 17:33:03 +0530

    2 files updated

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,12 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container ">
-    <div class="row justify-content-center">
+<div class="container bg-white ">
+
+    <div class="row justify-content-center bg-white">
+        <div class="col-md-6">
+            <img src="26.jpg" alt=""style="float:left;width:250px; height:250px;" class="mx-auto d-block"> 
+          </div>
+        Many a time, people face issues regarding breakdown of their vehicles at a road side. One of the reasons mentioned below causes this hassle. 
+        <ul>
+            <li>  Worn-out vehicle battery</li>
+            <li>   Damaged wheels and tyres </li>
+            <li>  Lost keys</li>
+            <li>  Failing of the starter motor </li>
+            <li>  Deterioration of the HT leads </li>
+        </ul>
        
+        Whatever the reason may be, when people faced such a situation, it is very difficult to find mechanics from nearby areas and it will take much more time and cost. 
             
 
-               
-    <div class="row justify-content-center" style="font-size:84px;">ON ROAD VEHICLE REPAIR</div>
+    </div>       
+    
 </div>
 @endsection
