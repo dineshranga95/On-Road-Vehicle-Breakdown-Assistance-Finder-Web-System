da81e1e336f99774184a95338f6c5f93647b2d36
dineshranga95 <dineshranga570@gmail.com>
2020-02-04 17:40:54 +0530

    one file modified

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -18,7 +18,36 @@
        
         Whatever the reason may be, when people faced such a situation, it is very difficult to find mechanics from nearby areas and it will take much more time and cost. 
             
+        <div class="row justify-content-center" >
+            <div class="col-10"style="font-size:40px; text-align:center;color:blue;">
+                Our Services 
+            </div> 
+            <div class="col-10"style="font-size:25px; text-align:center; ">
+              Quality and Functionality, Guaranteed
+            </div>                       
+            </div>
+            <div class="row justify-content-center mt-5">
+              <div class="col-md-2">
+                  
+              </div>
+              <div class="col-md-4">
+                  <img src="4.webp" alt="" style="float:left;height:250px; width:350px;" class="mx-auto d-block"> 
+                </div>
+                
+                <div class="col-md-4">
+                  <img src="6.webp" alt=""style="float:left;width:250px;height:250px;" class="mx-auto d-block"> 
+                </div>
 
+                <div class="col-md-4">
+                  <img src="7.webp" alt=""style="float:right;width:250px;" class="mx-auto d-block"> 
+                </div>
+                <div class="col-md-4">
+                  <img src="22.jpg" alt=""style="width:250px;height:250px;" class="mx-auto d-block"> 
+                </div>
+                <div class="col-md-4">
+                  <img src="26.jpg" alt=""style="float:left;width:250px; height:250px;" class="mx-auto d-block"> 
+                </div>
+                           
     </div>       
     
 </div>
